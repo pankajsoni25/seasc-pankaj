28ab3962fbc40cf81d86d46b9dbba080e28f8cae
pankaj <pankajsoni00100@gmail.com>
2022-12-02 00:24:53 +0530

    api working added

--- src/main/java/com/ups/seas/controller/UpsController.java
+++ src/main/java/com/ups/seas/controller/UpsController.java
@@ -1,28 +1,55 @@
 package com.ups.seas.controller;
 
-import com.ups.seas.exception.EmployeeNotFound;
-import com.ups.seas.model.UpsEmployee;
-import com.ups.seas.service.UpsService;
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.ups.seas.model.Employees;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @RequestMapping("/employee")
 @Slf4j
 public class UpsController {
 
-    @Autowired
-    UpsService upsService;
 
     @GetMapping("/getByEmployeeId")
-    public ResponseEntity<UpsEmployee> getEmployeeDetails(@RequestParam(name = "employeeId") String employeeId) throws EmployeeNotFound {
+    public ResponseEntity<Employees> getEmployeeDetails(@RequestParam(name = "employeeId") String employeeId) throws IOException, ExecutionException, InterruptedException {
         log.info("[UpsController][getEmployeeDetails] is called, employeeId: {} ", employeeId);
-        return ResponseEntity.ok(upsService.getEmployeeDetails(employeeId));
+
+        InputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccount.json");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        Firestore db = FirestoreClient.getFirestore();
+        db.listCollections().forEach(System.out::println);
+        DocumentReference documentReference = db.collection("employees").document(employeeId);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot documentSnapshot = future.get();
+        log.info("inside the FirebaseService method getEmployeeDetails() document : {}", documentSnapshot.exists());
+
+        Employees employee = null;
+        if (documentSnapshot.exists()) {
+            log.info("inside the FirebaseService came inside if");
+            employee = documentSnapshot.toObject(Employees.class);
+        }
+        return ResponseEntity.ok(employee);
 
     }
 
---
+++ src/main/java/com/ups/seas/model/Employees.java
@@ -0,0 +1,12 @@
+package com.ups.seas.model;
+
+import lombok.Data;
+
+@Data
+public class Employees {
+	private String empId;
+	private String empName;
+	private String email;
+	private String mobile;
+
+}
